5c70c8c9d876c4b8d7a032debd7fa23f7326667c
miguelvelez0824 <mivelezb@unal.edu.co>
2025-02-06 08:13:03 -0500

    Codigo clases gestionFinanciera

--- src/gestorAplicacion/gestionFinanciera/CuentaBancaria.py
+++ src/gestorAplicacion/gestionFinanciera/CuentaBancaria.py
@@ -1,2 +1,37 @@
 class CuentaBancaria:
-    pass
+    def __init__(self, idTitular, Saldo):
+        self._idTitular = idTitular
+        self._Saldo = Saldo
+
+    def ingresar (self, cant):
+        self._Saldo += cant
+
+    def retirar(self, cant):
+        if(cant > self._Saldo):
+             return False
+        else:
+            self._Saldo = self._Saldo - cant
+            return True
+    
+    def transferencia(self, Destino, cant):
+        if(cant <= self._Saldo):
+            self.retirar(cant)
+            Destino.ingresar(cant)
+            return True
+        else:
+            return False
+        
+    # Getters and Setters
+
+    def getIdTitular(self):
+        return self._idTitular
+
+    def setIdTitular(self, idTitular):
+        self._idTitular = idTitular
+
+    def getSaldo(self):
+        return self._Saldo
+
+    def setSaldo(self, Saldo):
+        self._Saldo = Saldo
+            
--- src/gestorAplicacion/gestionFinanciera/Tesoreria.py
+++ src/gestorAplicacion/gestionFinanciera/Tesoreria.py
@@ -1,2 +1,45 @@
+from gestionFinanciera.CuentaBancaria import CuentaBancaria
+
 class Tesoreria:
-    pass
+    def __init__(self, total, metaSemanal):
+        self.__dineroEnCaja = 0
+        self.__metaSemanal = metaSemanal
+        self.__cuenta = CuentaBancaria(1, 10000000)
+        self.__total = total
+    
+    def verificacionMeta(self):
+        return self.__total >= self.__metaSemanal
+
+    #Transferir dinero de la caja a la cuenta
+    def transferenciaFondos(self):
+        self.__cuenta.ingresar(self.__dineroEnCaja)
+        self.__dineroEnCaja = 0
+    
+    #pagar el sueldo base
+    def pagarSueldoBase(self, cuenta, cantidad):
+        self.__cuenta.transferencia(cuenta, cantidad)
+    
+    #Getters and Setters
+    def getDineroEnCaja(self):
+        return self.__dineroEnCaja
+    
+    def setDineroEnCaja(self, dinero):
+        self.__dineroEnCaja = dinero
+
+    def getCuenta(self):
+        return self.__cuenta
+    
+    def setCuenta(self, cuenta):
+        self.__cuenta = cuenta
+    
+    def getTotal(self):
+        return self.__total
+    
+    def setTotal(self, total):
+        self.__total = total
+
+    def getMetaSemanal(self):
+        return self.__metaSemanal
+    
+    def setMetaSemanal(self, metaSemanal):
+        self.__metaSemanal = metaSemanal
