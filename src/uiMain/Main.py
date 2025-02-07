8eb5d0071896343b369c4b498ad200df41a486f1
frncscp <franciscocerenporto@gmail.com>
2025-02-06 23:13:20 -0500

    Automatic Update

--- src/uiMain/Main.py
+++ src/uiMain/Main.py
@@ -12,19 +12,21 @@ class Main:
     @classmethod
     def initRoot(cls):
         cls.root = Tk()
-        cls.root.title("Teatro Escuela Carlos Mayolo")
         cls.root.geometry("1600x900") #16:9
+        cls.root.title("Teatro Escuela Carlos Mayolo")
 
+
+        #FRAME IZQUIERDO
         leftFrame = Frame(cls.root, borderwidth= 10, bg = "blue")
         leftFrame.place(relx = 0, rely = 0, relwidth = 0.5, relheight = 1)
+        ttk.Label(leftFrame, text= "Teatro Escuela Carlos Mayolo", font = "Calibri 24").pack()
         ttk.Label(leftFrame, text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin maximus volutpat tortor sit amet congue. Fusce pretium quam quam, eget blandit eros eleifend non.').pack()
         
 
+        #FRAME DERECHO
         rightFrame = Frame(cls.root, borderwidth= 10, bg = "green", width = 800, height = 900)
         rightFrame.place(relx = 0.5, rely = 0, relwidth = 0.5, relheight = 1)
         ttk.Button(rightFrame, text = "Dele a ver que pasa", command = cls.destroy).pack()
-
-
     
     @classmethod
     def runApp(cls):
