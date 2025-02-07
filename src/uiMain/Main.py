e8a3b1a25fe90c36f3a68c40e62d8110fa1a1abb
frncscp <franciscocerenporto@gmail.com>
2025-02-06 23:03:31 -0500

    add: inicio de desarrollo de la ventana raiz

--- src/uiMain/Main.py
+++ src/uiMain/Main.py
@@ -1,2 +1,36 @@
+import tkinter as tk
+from tkinter import Tk, Frame, ttk
+
 class Main:
-    pass
+    root = None
+
+    @classmethod
+    def destroy(cls):
+        cls.root.destroy()
+
+    #Esta función se encargará de inicializar todo lo referente a la ventana raíz
+    @classmethod
+    def initRoot(cls):
+        cls.root = Tk()
+        cls.root.title("Teatro Escuela Carlos Mayolo")
+        cls.root.geometry("1600x900") #16:9
+
+        leftFrame = Frame(cls.root, borderwidth= 10, bg = "blue")
+        leftFrame.place(relx = 0, rely = 0, relwidth = 0.5, relheight = 1)
+        ttk.Label(leftFrame, text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin maximus volutpat tortor sit amet congue. Fusce pretium quam quam, eget blandit eros eleifend non.').pack()
+        
+
+        rightFrame = Frame(cls.root, borderwidth= 10, bg = "green", width = 800, height = 900)
+        rightFrame.place(relx = 0.5, rely = 0, relwidth = 0.5, relheight = 1)
+        ttk.Button(rightFrame, text = "Dele a ver que pasa", command = cls.destroy).pack()
+
+
+    
+    @classmethod
+    def runApp(cls):
+        cls.initRoot()
+        cls.root.mainloop()
+
+
+if __name__ == "__main__":
+    Main.runApp()
