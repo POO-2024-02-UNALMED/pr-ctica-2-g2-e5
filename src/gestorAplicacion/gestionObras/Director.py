09e52c458fc826b40719ed8186999b5bbb0f80f5
pdanna632 <pdanna632@gmail.com>
2025-02-08 17:07:49 -0500

    Update Director.py

--- src/gestorAplicacion/gestionObras/Director.py
+++ src/gestorAplicacion/gestionObras/Director.py
@@ -1,2 +1,21 @@
-class Director:
-    pass
+class Director(Artista):
+    directors = []
+    def __init__(nombre, id, genero):
+        self.genero = genero
+        self.horario = []
+        super.__init__(nombre, id)
+        Teatro.getInstancia().directors.append(self)
+        Teatro.getInstancia().artistas.append(self)
+        directors.append(self)
+        genero.anadirDirector(self)
+    
+    def str(){
+        return "Nombre: " + self.nombre + "\n" + "Identificación: " + self.id + "\n" + "Género: " + self.genero
+    }
+    
+    def isDisponible(inicio, fin):
+        for evento in horario:
+            if inicio < evento[1] && fin > evento[0]:
+                return False
+                pass
+        return True 
