c57f0c257bd40dd01a2376f978545a7111fd68ae
juanmira19 <jmirac@unal.edu.co>
2025-02-16 14:55:51 -0500

    func_1

--- src/uiMain/Main.py
+++ src/uiMain/Main.py
@@ -383,8 +383,7 @@ class Main:
 
 
         def Usuario_Nuevo():
-            for widget in cls.content.winfo_children():
-                widget.destroy()
+
 
                 
             #SE CREARA UN NUEVO ID
@@ -426,10 +425,15 @@ class Main:
             frame_der = tk.Frame(cls.content, bg="blue")
             frame_der.place(relx=0.85, rely=0,relwidth=0.15, relheight=1)
 
-            frame_2 = tk.Frame(cls.content, bg="black", padx=15, pady=20)
-            frame_2.place(relx=0.5, rely=0.80,anchor="center")
             
-            frame = tk.Frame(cls.content, bg="white", padx=20, pady=20)
+
+            frame_central = tk.Frame(cls.content, bg="slategray1",name="central")
+            frame_central.place(relx=0.15, rely=0.1, relwidth=0.70, relheight=0.80)
+
+            frame_2 = tk.Frame(frame_central, bg="slategray2", padx=15, pady=20)
+            frame_2.place(relx=0.5, rely=0.85,anchor="center")
+            
+            frame = tk.Frame(frame_central, bg="slategray2", padx=20, pady=20)
             frame.place(relx=0.5, rely=0.5, anchor="center")
 
             top_frame = Frame(cls.content,background="black")
@@ -438,13 +442,12 @@ class Main:
             top_label = tk.Label(top_frame,text="Venta de tiquetes",font=("Calibri", 25), bg="black",fg="white")
             top_label.place(relx=0.5, rely=0.1, anchor="n")
 
-            pregunta1= FieldFrame(root=frame,tituloCriterios="Ingresa tu ID",combobox=True)
-            #pregunta1.pack()
+
 
             
 
         # Etiqueta
-            label = tk.Label(frame, text="Ingresa tu ID:", font=("Arial", 14), bg="white")
+            label = tk.Label(frame, text="Ingresa tu ID:", font=("Arial", 14), bg="slategray3")
             label.pack(pady=10)
 
         # Cuadro de texto
@@ -459,6 +462,8 @@ class Main:
 
             
         def Inicio_preguntas():
+            for widget in cls.content.winfo_children():
+                widget.destroy()
             global frame_central
             
             frame_izq = tk.Frame(cls.content, bg="blue")
@@ -471,16 +476,15 @@ class Main:
             top_frame = Frame(cls.content,background="black")
             top_frame.place(relx=0, rely=0, relwidth= 1, relheight= 0.1)
 
-            frame_central = tk.Frame(cls.content, bg="white")
+            frame_central = tk.Frame(cls.content, bg="slategray1",name="central")
             frame_central.place(relx=0.15, rely=0.1, relwidth=0.70, relheight=0.80)
 
-            frame_central = tk.Frame(cls.content, bg="white")
-            frame_central.place(relx=0.15, rely=0.1, relwidth=0.70, relheight=0.80)
+
 
             top_label = tk.Label(top_frame,text="Venta de tiquetes",font=("Calibri", 25), bg="black",fg="white")
             top_label.place(relx=0.5, rely=0.1, anchor="n")
 
-            label = tk.Label(cls.content,text="Desea mejorar su suscripcion?", font=("Calibri", 25), fg="black",bg="white",name="suscripcion")
+            label = tk.Label(cls.content,text="Desea mejorar su suscripcion?", font=("Calibri", 25), fg="black",bg="slategray2",name="suscripcion")
             label.place(relx=0.5, rely=0.3, anchor="center")
 
             Button_Si = tk.Button(cls.content, text="Si", font=("Calibri", 15),command=adquirir_suscripcion,name="no")
@@ -514,8 +518,6 @@ class Main:
                 command=lambda :asignar_suscripcion(susc)
                 
             )
-            
-            
             susc.place(relheight= 1, relwidth= 1)
             main_label = tk.Label(frame_central,bg = "slategray1",text="BÁSICA $0.00 -------\n\nPREMIUM \n$11,900.00\n 10% de Descuento\nEN TODAS LAS FUNCIONES\nY ASIENTOS\n\nVIP \n$18,900.00\n 25% de Descuento\nEN TODAS LAS FUNCIONES\nY ASIENTOS\n\nELITE \n$39,900.00 \nFUNCIONES GRATIS\nILIMITADAS Y\nASIENTO GOLD GRATIS")
             main_label.place(relx=0.53, rely=0.5, anchor="center")
@@ -535,7 +537,9 @@ class Main:
                 cliente.set_suscripcion(Suscripcion.ELITE.value)
             else:
                 raise ValueError("Suscripción no válida")
+            
             print(cliente.get_suscripcion())
+            continuar()
         
    
 
@@ -547,6 +551,14 @@ class Main:
 
         def continuar():
             try:
+                widget = cls.content.nametowidget("central") 
+                widget.destroy()
+
+            except KeyError:
+                print("error frame")
+            try:
+                widget = cls.content.nametowidget("central") 
+                widget.destroy()
                 widget = cls.content.nametowidget("si") 
                 widget.destroy()
                 widget = cls.content.nametowidget("no") 
@@ -554,7 +566,9 @@ class Main:
                 widget = cls.content.nametowidget("suscripcion") 
                 widget.destroy()
             except KeyError:
-                None
+                print("error label")
+            frame_central = tk.Frame(cls.content, bg="slategray1",name="central")
+            frame_central.place(relx=0.15, rely=0.1, relwidth=0.70, relheight=0.80)
             
 
 
@@ -578,10 +592,10 @@ class Main:
         top_frame = Frame(cls.content,background="black")
         top_frame.place(relx=0, rely=0, relwidth= 1, relheight= 0.1)
 
-        frame_central = tk.Frame(cls.content, bg="white")
+        frame_central = tk.Frame(cls.content, bg="slategray1")
         frame_central.place(relx=0.15, rely=0.1, relwidth=0.70, relheight=0.80)
 
-        frame_central = tk.Frame(cls.content, bg="white")
+        frame_central = tk.Frame(cls.content, bg="slategray1")
         frame_central.place(relx=0.15, rely=0.1, relwidth=0.70, relheight=0.80)
 
         top_label = tk.Label(top_frame,text="Venta de tiquetes",font=("Calibri", 25), bg="black",fg="white")
@@ -592,7 +606,7 @@ class Main:
             lambda e: cls.resize(top_frame,top_label, 60, 100)
         )"""
 
-        label = tk.Label(cls.content,text="Eres un cliente nuevo?", font=("Calibri", 25), fg="black",bg="white")
+        label = tk.Label(cls.content,text="Eres un cliente nuevo?", font=("Calibri", 25), fg="black",bg="slategray1")
         label.place(relx=0.5, rely=0.3, anchor="center")
 
         Button_Si = tk.Button(cls.content, text="Si", font=("Calibri", 15),command=Usuario_Nuevo)
